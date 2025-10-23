f50fdb70916a6b885083fcf86fad51a4f5ec0ad1
pmarc <marc.pouyalet@ynov.com>
2025-10-23 16:43:51 +0200

    duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -1,3 +1,3 @@
-let sentence = Hello there
-let duplicate = ${sentence}, ${sentence} !
-console.log(duplicate);
+let sentence = Hello there;
+let duplicate = `${sentence}, ${sentence} !`;
+console.log(duplicate)
