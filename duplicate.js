263e6c76ca63b9014eeb47077e0100f6d138f1c2
pmarc <marc.pouyalet@ynov.com>
2025-10-23 17:03:54 +0200

    duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -1,4 +1,3 @@
-let sentence = "I told you so";
+let sentence = I told you so;
 let duplicate = `${sentence}, ${sentence}, ${sentence} !`;
 console.log(duplicate);
-// Résultat : I told you so, I told you so, I told you so !
