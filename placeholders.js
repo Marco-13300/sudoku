a8c11563d5738d5f84685a9172c65f43e0bad5b8
pmarc <marc.pouyalet@ynov.com>
2025-10-23 16:35:16 +0200

    placeholders.js

--- placeholders.js
+++ placeholders.js
@@ -1,2 +1,4 @@
-let presentation =`Hello my name is ${name} and I'm ${age} years old`;
+let Myname = Marc
+let Myage = 38
+let presentation =`Hello my name is ${Myname} and I'm ${Myage} years old`;
 console.log(presentation)
