cd5801c6e413a36fcd6489b51bd81fa359699f9c
pmarc <marc.pouyalet@ynov.com>
2025-11-28 10:16:38 +0100

    bring-it-to-life

--- bring-it-to-life.js
+++ bring-it-to-life.js
@@ -1,21 +1,3 @@
-const lifeSwitch = document.getElementById('life-switch-ball'); 
-const torso = document.getElementById('torso');
-const eyeContainer = document.getElementById('eye-container'); 
-
-function toggleLife() {
-    
-    lifeSwitch.classList.toggle('switch-on');
-
-    eyeContainer.classList.toggle('eye-open');
-    eyeContainer.classList.toggle('eye-closed'); 
-
-    if (lifeSwitch.classList.contains('switch-on')) {
-        torso.style.transform = 'translateY(-10px)'; 
-        torso.style.boxShadow = '0 0 15px rgba(255, 255, 255, 0.5)';
-    } else {
-        torso.style.transform = 'translateY(0)';
-        torso.style.boxShadow = 'none';
-    }
-}
-
-lifeSwitch.addEventListener('click', toggleLife);
+const leftEye = document.getElementById('eye-left');
+leftEye.style.backgroundColor = 'black';
+leftEye.classList.add('eye-closed');
